f80d1044bb826eac84e831872a0a25d94766a7da
amisha bisht <amishabisht33@gmail.comgit status>
2022-02-17 22:58:57 +0530

    excercism solutions

---
+++ two_fer.py
@@ -0,0 +1,25 @@
+'''Two-fer or 2-fer is short for two for one. One for you and one for me.
+
+Given a name, return a string with the message:
+
+One for name, one for me.
+Where "name" is the given name.
+
+However, if the name is missing, return the string:
+
+One for you, one for me.
+Here are some examples:
+
+Name	String to return
+Alice	One for Alice, one for me.
+Bob	One for Bob, one for me.
+One for you, one for me.
+Zaphod	One for Zaphod, one for me.'''
+name=str(input("enter name "))
+def two_fer(name=None):
+    if name:
+        return f"One for {name}, one for me."
+    else:
+        return f"One for you, one for me."
+
+two_fer(name)
